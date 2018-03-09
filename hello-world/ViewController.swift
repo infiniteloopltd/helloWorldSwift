9ea0a0d453b1a0b218878b1747ffe84d56fb5d09
user909680 <user909680@EU228.local>
2018-03-09 15:53:26 +0000

    Initial Commit

--- hello-world/ViewController.swift
+++ hello-world/ViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfName: UITextField!
+    
+    @IBOutlet weak var tvOutput: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func btnGo_Click(_ sender: Any) {
+        tvOutput.isHidden = false
+        tvOutput.text = "Hello " + tfName.text!
+        
     }
-
-
+    
 }
 
