25de96840bf1120d1538def933f472b4138be184
Infinite Loop Development Ltd <fiach_reid@hotmail.com>
2018-03-16 17:43:58 +0000

    Update ViewController.swift

--- hello-world/ViewController.swift
+++ hello-world/ViewController.swift
@@ -34,7 +34,7 @@ class ViewController: UIViewController {
         do
         {
             
-             let dict = try german_lookup(registrationNumber: tfName.text!,  username:"xxxxx", password:"lokiju")
+             let dict = try german_lookup(registrationNumber: tfName.text!,  username:"xxxxx", password:"xxxxx")
             
             tvOutput.text = dict["Description"] as? String
             let strImageUrl = dict["ImageUrl"] as? String
